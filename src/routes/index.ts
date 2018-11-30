56166c0a76773a452a89f8d93eadc1c0e999c0d1
igorkhorev <igorhorev@gmail.com>
2018-11-30 13:36:33 +0300

    fix: index welcome message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   // res.render('index', { title: 'Express !!!' });
-  res.send('*********************');
+  res.send('Hello! I am a Mapper server.');
 });
 
 export default router;
