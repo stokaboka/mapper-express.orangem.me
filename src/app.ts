5b54aab3473b5eae71a7132b1bc43825b0da92de
igorkhorev <igorhorev@gmail.com>
2018-11-30 14:05:51 +0300

    fix: path to publci dir

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
