d0fe074e3a6c572e111d61661d3889ee73b5fa26
igorkhorev <igorhorev@gmail.com>
2018-11-30 13:31:31 +0300

    fix: move project code from js to ts

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import log4js from 'log4js';
+import path from 'path';
+
+import dataProviderRouter from './routes/dataProvider';
+import indexRouter from './routes/index';
+import mapperRouter from './routes/mapper';
+
+const log = log4js.getLogger('app');
+
+const app = express();
+
+app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use('/', indexRouter);
+app.use('/mapper', mapperRouter);
+app.use('/dp', dataProviderRouter);
+
+export default app;
